491b08bed517f80b69f29301cd57c33e862fc3b5
Arif9940 <arifhossen9940@gmail.com>
2023-07-26 21:47:57 +0600

    task 2

--- main.js
+++ main.js
@@ -7,5 +7,25 @@
     }
     return newString;
 }
- const result = reverseStringWithoutMethod("Mozilla");
+ const result = reverseStringWithoutMethod("hello world");
   console.log(result);
+
+//   Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
+
+
+const sumPositveNum =(arr)=>{
+    let negatives = [];
+   let sum =0;
+   for(let i = 0; i<arr.length; i++){
+    if(arr[i] < 0) {
+        negatives.push(arr[i]);
+      }else{
+        sum += arr[i];
+      }
+   }
+   return sum;
+}
+const totalOfAllPositiveNum = sumPositveNum([12,-2,-3,4])
+console.log(totalOfAllPositiveNum);
+
+
